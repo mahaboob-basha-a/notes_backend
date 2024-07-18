84b135ff796bd6456b534a88857bf47ec9aec5fa
mahaboob-basha-a <alurumahaboobbasha@gmail.com>
2024-07-18 20:35:05 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/notes-app');
+    await mongoose.connect('mongodb+srv://telugutech62:Basha9515@practiceone.xetfdo7.mongodb.net/?retryWrites=true&w=majority&appName=PracticeOne');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
